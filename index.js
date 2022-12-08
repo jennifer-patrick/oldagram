c20526b53f74c364c41dcb73478b076cd5be9829
Jennifer Patrick <118204029+jennifer-patrick@users.noreply.github.com>
2022-12-08 18:56:42 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+const nameEl = document.getElementById("name")
+const usernameEl = document.getElementById("username")
+const locationEl = document.getElementById("location")
+const avatarEl = document.getElementById("avatar")
+const postEl = document.getElementById("post-img")
+const commentEl = document.getElementById("comment")
+const likesEl = document.getElementById("likes")
+
+const likeBtn = document.getElementById("like-btn")
+
+
+for (let i=0; i<posts.length;i++){
+    nameEl.textContent = posts[i].name 
+    usernameEl.textContent = posts[i].username
+    locationEl.textContent = posts[i].location
+    avatarEl.src = posts[i].avatar
+    postEl.src = posts[i].post
+    commentEl.textContent = posts[i].comment
+    likesEl.textContent = `${posts[i].likes} likes`
+    
+    likeBtn.addEventListener("click", function() {
+        likesEl.textContent = posts[i].likes+1+ " likes"
+        
+    })
+
+}
+
+
+
+
+// console.log( Math.floor( Math.random()*3) )
+
+
+    
